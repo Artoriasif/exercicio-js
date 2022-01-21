3887517ac70a10770cee06d374c0f2f302b0c825
Pedro <pedrohh0012@icloud.com>
2022-01-21 17:40:33 -0300

    Acerto no estilo

--- exercicios/exercicio1/script.js
+++ exercicios/exercicio1/script.js
@@ -3,19 +3,20 @@ function carregar() {
   var foto = window.document.getElementById("imagem");
   var data = new Date();
   var hora = data.getHours();
-  msg.innerHTML = `Agora são ${hora} horas.`;
+  var minutos = data.getMinutes();
+  msg.innerHTML = `Agora são ${hora} horas e ${minutos} minutos.`;
 
   if (hora >= 0 && hora < 12) {
     //BOM DIA
     foto.src = "img/manha.jpeg";
-    document.body.style.background = "#e2cd9f";
+    document.body.style.background = "#e6cf60";
   } else if (hora >= 12 && hora < 18) {
     //BOA TARDE
     foto.src = "img/tarde.jpeg";
-    document.body.style.background = "#b9846f";
+    document.body.style.background = "#5cbbbd";
   } else {
     //BOA NOITE
     foto.src = "img/noite.jpeg";
-    document.body.style.background = "#515154";
+    document.body.style.background = "#2d3333";
   }
 }
